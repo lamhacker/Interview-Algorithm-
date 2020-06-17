7fd80cb73e7c4eabbc231c5023660443d2b0d2f3
Frank Lan <lanyufeng123@gmail.com>
2020-06-17 21:09:20 +1000

    day1

---
+++ BubbleSort.java
@@ -0,0 +1,24 @@
+import java util.Arrays;
+
+//选arr中最大的作为最右边，下一次忽略最后一个，对arr-1进行排序
+public class BubbleSort() {
+
+	public static void bubbleSort(int[] arr) {
+		if (arr == null || arr.length < 2) {
+			return;
+		}
+		for (int end = arr.length - 1; end > 0; end--) {
+			for (int i = 0; i < end; i++) {
+				if (arr[i] > arr[i + 1]) {
+					swap(arr, i, i+1);
+				}
+			}
+		}
+	}
+
+	public static void swap(int[] arr, int i, int j) {
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+}
