f6e8569dd71cafda4ee3bd03c4de57e61c641d15
Frank Lan <lanyufeng123@gmail.com>
2020-06-17 21:21:57 +1000

    day1

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,6 +1,7 @@
-import java util.Arrays;
+import java.util.Arrays;
 
 //选arr中最大的作为最右边，下一次忽略最后一个，对arr-1进行排序
+//如果当前元素大于下一个元素，交换顺序，这样下一个元素会比前一个元素大，最右边的元素会变成整个数组最大的
 public class BubbleSort() {
 
 	public static void bubbleSort(int[] arr) {
