998996d31699b4718bad81c65f6d439cdf3fe5a8
Frank Lan <lanyufeng123@gmail.com>
2020-06-20 20:55:30 +1000

    day4

---
+++ NetherlandsFlag.java
@@ -0,0 +1,18 @@
+public class NetherlandsFlag {
+
+	public static int[] partition(int[] arr, int l, int r, int p) {
+		int less = l - 1;
+		int more = r + 1;
+		while (l < more) {
+			if (arr[l] < p) {
+				swap(arr, ++less, l++);
+			} else if (arr[l] > p) {
+				swap(arr, --more, l);
+			} else {
+				l++;
+			}
+		}
+		return new int[] { less + 1, more - 1 };
+	}
+
+}
