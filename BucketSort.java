ce88b00b2c95e94a3c82342c0f0602c8d7ea19dc
Frank Lan <lanyufeng123@gmail.com>
2020-06-20 20:52:57 +1000

    day3

---
+++ BucketSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class BucketSort {
+
+	// only for 0~200 value
+	public static void bucketSort(int[] arr) {
+		if (arr == null || arr.length < 2) {
+			return;
+		}
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < arr.length; i++) {
+			max = Math.max(max, arr[i]);
+		}
+		int[] bucket = new int[max + 1];
+		for (int i = 0; i < arr.length; i++) {
+			bucket[arr[i]]++;
+		}
+		int i = 0;
+		for (int j = 0; j < bucket.length; j++) {
+			while (bucket[j]-- > 0) {
+				arr[i++] = j;
+			}
+		}
+	}
+
+	
